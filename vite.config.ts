bd8fd180ac901e8d71130d384a3085ffbb0c6443
Josue19-08 <josuemarin2009@hotmail.com>
2025-11-07 23:36:27 -0600

    chore: configure vite with alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,16 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [
-      react(),
-      nodePolyfills({
-        include: ["buffer"],
-        globals: {
-          Buffer: true,
-        },
-      }),
-      wasm(),
-    ],
-    build: {
-      target: "esnext",
-    },
-    optimizeDeps: {
-      exclude: ["@stellar/stellar-xdr-json"],
-    },
-    define: {
-      global: "window",
-    },
-    envPrefix: "PUBLIC_",
-    server: {
-      proxy: {
-        "/friendbot": {
-          target: "http://localhost:8000/friendbot",
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
+
